e02f83ec6bf4715413dbc302887d53449bceb846
frabrenn <frabrenn@amazon.com>
2023-01-02 10:57:55 -0700

    non-AR stow master matching legacy vol/hrs, fullness, and vet pct

--- olympus_dbt/models/marts/master/master_stow_non_ar.sql
+++ olympus_dbt/models/marts/master/master_stow_non_ar.sql
@@ -1,10 +1,10 @@
 
 /*
-    This script aggregates all the non-AR stow olympus metrics into one table -- master_stow_non_ar
+    This script aggregates all the non-AR stow olympus metrics into one view -- master_stow_non_ar
 */
 
 with stow_tbb_bee as (
-    select * from {{ref('core_pick_tbb_bee')}}
+    select * from {{ref('core_stow_tbb_bee')}}
 ),
 
 fullness as ( -- fullness has more granularities than stow tbb (it also has mod and is_locked dimensions)
@@ -37,18 +37,19 @@ load_table as (
         s.container_type,
         s.volume as act_vol,
         s.secs/3600.0 as act_hrs,
+        act_vol*1.0/nullif(act_hrs,0) as act_uph,
         f.cube,
         f.capacity,
         f.cube*1.0/nullif(f.capacity,0) as fullness_pct,
         case
             when f.cube = 0 then 'low'
             when f.capacity = 0 then 'low'
-            when f.cube / f.capacity < 0.5 then 'low'
-            when f.cube / f.capacity < 0.8 then 'med'
-            when f.cube / f.capacity < 0.9 then 'high'
-            when f.cube / f.capacity < 0.95 then 'veryhigh'
-            when f.cube / f.capacity < 1 then 'reallyhigh'
-            when f.cube / f.capacity < 1.1 then 'reallyreallyhigh'
+            when fullness_pct < 0.5 then 'low'
+            when fullness_pct < 0.8 then 'med'
+            when fullness_pct < 0.9 then 'high'
+            when fullness_pct < 0.95 then 'veryhigh'
+            when fullness_pct < 1 then 'reallyhigh'
+            when fullness_pct < 1.1 then 'reallyreallyhigh'
             else 'extremelyhigh' 
         end::varchar(55)                                            as fullness_type,
         vet_emps,
